21d380bde7405ded5663ccbd7b38d8af7980a9fa
houance <837507557@qq.com>
2022-01-04 17:07:49 +0000

    1.change client logic init connection-nonrelate variable first

--- client/Client.py
+++ client/Client.py
@@ -2,22 +2,22 @@ import socket
 from utils.NetTransfer import NetTransfer
 from client.BinaryFramer import BinaryFramer
 from yuNet import Header, Request, Response
+from time import sleep
 
 
 class Client:
     def __init__(self, address:str, port:int, streamID=1) -> None:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((address, port))
-        self.reader = client.makefile('rb')
-        self.writer = client.makefile('wb')
-
         self.header = Header()
         if streamID <= 0 or streamID >= 4294967295:
             raise ValueError("StreamID Must Greater Than Zero and Smaller than 2^32 -1")
         self.header.streamID = streamID
-
         self.request = Request()
         self.response = Response()
+
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((address, port))
+        self.reader = client.makefile('rb')
+        self.writer = client.makefile('wb')
         
     def send(self, frame):
         frame = NetTransfer.resize(frame)
